ec86e74a07a5e8a1866039d9a0b27dcfc6e19845
jpratty <japratt1999@gmail.com>
2020-11-09 14:58:20 -0700

    Added multiple rolls and total eq

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,12 @@
+import random
 def main():
-  #print('You rolled a die')
+    dice_rolls=2
+    roll_sum = 0
+    for i in range(0,dice_rolls):
+        roll= random.randint(1,6)
+        print(f'You rolled a {roll}')
+        roll_sum += roll
+    print(f'Your total is {roll_sum}')
 
 if __name__== "__main__":
   main()
